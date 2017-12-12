1f4be1cb4ffe22604947367b62f57890c60fe94b
Mosen <mosen@users.noreply.github.com>
2017-12-12 16:59:41 +1100

    ditto

--- Adobe/CreativeCloudBuildModifier.py
+++ Adobe/CreativeCloudBuildModifier.py
@@ -132,8 +132,9 @@ class CreativeCloudBuildModifier(Processor):
 
         for to_remove in packages_to_remove:
             remove_pkg = acc_packageset.find(".//package[name='{}']".format(to_remove))
-            if remove_pkg:
+            if remove_pkg is not None:
                 self.output('Removing package {}'.format(to_remove))
+                asu_appinfo_root.remove(remove_pkg)
             else:
                 self.output('Could not find package "{}" to remove.'.format(to_remove))
 
