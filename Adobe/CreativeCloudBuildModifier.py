272cd9251c92b403c785027ea470c34b9b680951
Mosen <mosen@users.noreply.github.com>
2017-12-12 16:56:48 +1100

    attempting to find the right xml xpath expression

--- Adobe/CreativeCloudBuildModifier.py
+++ Adobe/CreativeCloudBuildModifier.py
@@ -134,6 +134,8 @@ class CreativeCloudBuildModifier(Processor):
             remove_pkg = acc_packageset.find("./package[name='{}']".format(to_remove))
             if remove_pkg:
                 self.output('Removing package {}'.format(to_remove))
+            else:
+                self.output('Could not find package "{}" to remove.'.format(to_remove))
 
     # <ACCPanelMaskingConfig>
     # <config>
